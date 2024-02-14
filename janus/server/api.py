e241fe8351fcdcb894ea2ef142ffb1728d461a38
Christian Oertlin <c.oertlin@gmail.com>
2024-02-14 15:28:09 +0100

    fix request type

--- janus/server/api.py
+++ janus/server/api.py
@@ -7,7 +7,7 @@ from janus.dto.collect_qc_response import CollectQCResponse
 collect_qc_router = APIRouter()
 
 
-@collect_qc_router.post(
+@collect_qc_router.get(
     "/collect_qc/",
     response_description="Collect qc metrics for a case.",
     response_model=CollectQCResponse,
