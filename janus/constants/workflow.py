e1182594be14246471c0e711c84fbc1335909f8d
Christian Oertlin <c.oertlin@gmail.com>
2024-03-04 14:20:45 +0100

    fix exception

--- janus/constants/workflow.py
+++ janus/constants/workflow.py
@@ -7,3 +7,7 @@ class Workflow(StrEnum):
     BALSAMIC: str = "balsamic"
     BALSAMIC_QC: str = "balsamic-qc"
     BALSAMIC_UMI: str = "balsamic-umi"
+
+    @classmethod
+    def list(cls):
+        return list(map(lambda c: c.value, cls))
