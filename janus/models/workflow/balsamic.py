eb1ad2c04b390f53a4fb201078f9ab095abacfc1
Christian Oertlin <c.oertlin@gmail.com>
2024-03-12 09:54:07 +0100

    default none

--- janus/models/workflow/balsamic.py
+++ janus/models/workflow/balsamic.py
@@ -31,5 +31,5 @@ class BalsamicSample(BaseModel):
 
 class Balsamic(BaseModel):
     samples: list[BalsamicSample]
-    somalier: Somalier | None
+    somalier: Somalier | None = Field(None)
     workflow: WorkflowInfo
