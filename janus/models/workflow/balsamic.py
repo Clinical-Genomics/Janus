afbd809fb9807f8166eb52c3a5fe199d9f163df9
ChristianOertlin <c.oertlin@gmail.com>
2024-03-12 11:46:02 +0100

    Update janus/models/workflow/balsamic.py
    Co-authored-by: Vadym <vadym.ivanchuk@scilifelab.se>

--- janus/models/workflow/balsamic.py
+++ janus/models/workflow/balsamic.py
@@ -31,5 +31,5 @@ class BalsamicSample(BaseModel):
 
 class Balsamic(BaseModel):
     samples: list[BalsamicSample]
-    somalier: Somalier | None = Field(None)
+    somalier: Somalier | None = None
     workflow: WorkflowInfo
